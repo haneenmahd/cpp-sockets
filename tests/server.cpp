5122737be9bb6600e98a6a73243b7153812e34d8
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-25 17:50:51 +0530

    add more functions

--- tests/server.cpp
+++ tests/server.cpp
@@ -5,7 +5,14 @@ int main()
 {
     Socket socket(8080, "Hello World");
 
+    socket.initSocket();
+
+    socket.attachToPort();
+    socket.setAddressConfiguration();
+    socket.bindSocket();
+
     socket.listenSocket();
     socket.acceptNewSocket();
+
     return 0;
 }
