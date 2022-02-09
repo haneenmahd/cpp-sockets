52f233ca5e5ec923112dabaf7c118d9c90a0808d
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-09 11:48:54 +0530

    test sockets

--- tests/SocketTests.test.cpp
+++ tests/SocketTests.test.cpp
@@ -4,6 +4,7 @@
 
 int main()
 {
+    // client testing
     SocketClient client(3000, "Test Client");
 
     expect_int("Expect state to be initialised", client.state, 3);
@@ -11,6 +12,31 @@ int main()
     client.setAddressConfiguration();
 
     expect_int("Expect state to be initialised", client.state, 3);
+
+    // server testing
+    Socket serv(3000, "Test Server");
+
+    expect_int("Expect the state to be uninitialised", serv.state, 2);
+
+    serv.initSocket();
+
+    expect_int("Expect the state to be initialised", serv.state, 3);
+
+    serv.attachToPort();
+
+    expect_int("Expect the state to be initialised", serv.state, 2);
+
+    serv.setAddressConfiguration();
+
+    expect_int("Expect the state to be initialised", serv.state, 2);
+
+    serv.bindSocket();
+
+    expect_int("Expect the state to be initialised", serv.state, 2);
+
+    serv.listenSocket();
+
+    expect_int("Expect the state to be alive", serv.state, 1);
 
     return 0;
 }
